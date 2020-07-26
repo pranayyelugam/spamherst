c3964bed84261dbb224ff3298b5937cece121a86
Pranay Yelugam <shruthiy@SHRUTHIYs-MacBook-Pro.local>
2020-07-26 23:35:23 +0530

    updated client.js

--- client.js
+++ client.js
@@ -50,21 +50,56 @@ client.on('guildMemberAdd', member => {
 })
 
 function processCommand(message) {
-    let command = message.content.substr(1)
     const guild = client.guilds.cache.get(guildId)
+
+    let command = message.content.substr(1)
+    let splitCommand = command.split(" ") // Split the message up in to pieces for each space
+    let primaryCommand = splitCommand[0] // The first word directly after the exclamation is the command
+    let arguments = splitCommand.slice(1) // All other words are arguments/parameters/options for the command
+
+    switch (primaryCommand) {
+        case "createRole":
+            // !createrole Employed BLUE
+            if (message.member.hasPermission('MANAGE_GUILD')) {
+                if (arguments.length < 0) return
+                if (arguments.length > 2) message.channel.send("Please enter the command correctly. Check #general-info for help")
+                guild.roles.create({
+                    data: {
+                        name: arguments[0],
+                        color: arguments[1]
+                    }
+                }).then(console.log("Employed role successfully created for user: " + message.author))
+                    .catch(console.error("Employed role can't be created for: " + message.author))
+                message.react('👍')
+            }
+            else {
+                message.channel.send("You do not have the permission to create a role").then(sentMessage => {
+                    sentMessage.react(':thumbsdown_tone1:');
+                });
+            }
+        case "addRole":
+            if (arguments.length < 0) return
+            if (arguments.length > 1) message.channel.send("Please enter the command correctly. Check #general-info for help")
+            if (arguments[0] == "Employed") {
+                const member = message.mentions.members.first();
+                if (member.roles.cache.some(role => role.name === 'Employed')) {
+                    message.channel.send("You already have the Employed badge. :(").then(sentMessage => {
+                        sentMessage.react(':thumbsdown_tone1:');
+                    });
+                } else {
+                    const role = guild.roles.cache.find(role => role.name === 'Employed');
+                    if (!role) return console.error("404: role not found")
+                    member.roles.add(role)
+                    message.react('👍')
+                }
+            }
+    }
+
 
     switch (command) {
         case "ping":
             message.channel.send("pong")
-        case "employed":
-            guild.roles.create({
-                data: {
-                    name: 'Employed',
-                    color: 'BLUE'
-                }
-            }).then(console.log("Employed role successfully created for user: " + message.author))
-                .catch(console.error("Employed role can't be created for: " + message.author))
-            message.channel.send("Badge added successfully")
+
     }
 
 }
