4d3d79c5520929f03b6d9d63bde3b11f3d0c77d1
Pranay Yelugam <shruthiy@SHRUTHIYs-MacBook-Pro.local>
2020-07-26 17:22:32 +0530

    added bot logs channel

--- client.js
+++ client.js
@@ -4,19 +4,21 @@ const config = require("./config/config.json")
 
 
 client.on('ready', () => {
-    console.log("Spamherst is online!");
-});
+    console.log("Spamherst is online!")
+    client.channels.get('736913317020434522').send("Spamherst is Online!")
+})
 
 client.on('message', msg => {
     if (msg.content == 'ping') {
         console.log('pong')
     }
-});
+})
 
 client.on('guildMemberAdd', member => {
-    member.guild.channels.get('736909338425294899').send('**' + member.user.username + '**, has joined the server!')
+    client.channels.get('736909338425294899').send('**' + member.user.username + '** has joined the server!')
         .then(send("We hope you have brought pizza with you!"))
-});
+})
+
 
 
 client.login(config.DISCORD_BOT.TOKEN)
