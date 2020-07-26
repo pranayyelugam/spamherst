e1584a5a694d7fe8d3b35ea0f62f60daac5680cf
Pranay Yelugam <shruthiy@SHRUTHIYs-MacBook-Pro.local>
2020-07-26 19:46:15 +0530

    added command option

--- client.js
+++ client.js
@@ -10,16 +10,21 @@ client.on('ready', () => {
     client.channels.cache.get('736913317020434522').send("Spamherst is Online!")
 })
 
-client.on('message', msg => {
+client.on('message', message => {
+    if (message.author == client.user) {
+        return // Prevent bot from responding to its own messages
+    }
+
+    if (message.content.startsWith('!')) {
+        processCommand(message)
+    }
+
     if (msg.content == 'addroles') {
         const guild = client.guilds.cache.get(guildID);
         const role = guild.roles.cache.get('736909983773491242')
         if (!role) return console.error("404: role not found")
         guild.members.fetch().then(members => {
             members.forEach(m => {
-                if (m.id == client.id) { // Prevent bot from responding to its own messages
-                    return
-                }
                 m.roles.add(role)
             })
 
@@ -42,6 +47,15 @@ client.on('guildMemberAdd', member => {
 
 })
 
+function processCommand(message) {
+    let command = message.content.substr(1)
+
+    switch (command) {
+        case "ping":
+            message.channel.send("pong")
+    }
+
+}
 
 
 client.login(config.DISCORD_BOT.TOKEN)
