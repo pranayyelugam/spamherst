44d8169e44d8f251ccf69831628e94437a6c3e25
Pranay Yelugam <shruthiy@SHRUTHIYs-MacBook-Pro.local>
2020-07-26 17:13:41 +0530

    add welcome message

--- client.js
+++ client.js
@@ -4,7 +4,19 @@ const config = require("./config/config.json")
 
 
 client.on('ready', () => {
-    console.log("hello");
+    console.log("Spamherst is online!");
 });
+
+client.on('message', msg => {
+    if (msg.content == 'ping') {
+        console.log('pong')
+    }
+});
+
+client.on('guildMemberAdd', member => {
+    member.guild.channels.get('736909338425294899').send('**' + member.user.username + '**, has joined the server!')
+        .then(send("We hope you have brought pizza with you!"))
+});
+
 
 client.login(config.DISCORD_BOT.TOKEN)
