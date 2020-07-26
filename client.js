bfd0bbe6a8759e8cf0c35ad525682cf4e3584b9a
Pranay Yelugam <shruthiy@SHRUTHIYs-MacBook-Pro.local>
2020-07-27 02:12:33 +0530

    updated client.js

--- client.js
+++ client.js
@@ -47,6 +47,7 @@ function processCommand(message) {
     const guild = client.guilds.cache.get(guildId)
 
     const wholeCommand = message.content.substr(1)
+    const args = message.content.slice(prefix.length).trim().split(' ');
     const splitCommand = wholeCommand.split(" ") // Split the message up in to pieces for each space
     const commandName = splitCommand.shift().toLowerCase() // The first word directly after the exclamation is the command
     const arguments = splitCommand.slice(1) // All other words are arguments/parameters/options for the command
@@ -54,9 +55,9 @@ function processCommand(message) {
     if (!client.commands.has(commandName)) return
 
     const command = client.commands.get(commandName)
-    console.log(arguments)
-    console.log(arguments.length)
-    if (command.args && !arguments.length) {
+    console.log(args)
+    console.log(args.length)
+    if (command.args && !args.length) {
         let reply = `You didn't provide any arguments, ${message.author}!`;
         if (command.usage) {
             reply += `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``;
