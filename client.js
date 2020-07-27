a770fe6b4dfa2e86da3c290bae7db7ddca82b7da
Pranay Yelugam <shruthiy@SHRUTHIYs-MacBook-Pro.local>
2020-07-27 18:17:51 +0530

    updated client.js

--- client.js
+++ client.js
@@ -14,6 +14,7 @@ for (const file of commandFiles) {
     client.commands.set(command.name, command);
 }
 
+const cooldowns = new Discord.Collection();
 const guildId = "736864547889217637"
 
 client.on('ready', () => {
@@ -62,6 +63,24 @@ function processCommand(message) {
         }
         return message.channel.send(reply);
     }
+
+    if (!cooldowns.has(command.name)) {
+        cooldowns.set(command.name, new Discord.Collection());
+    }
+
+    const now = Date.now();
+    const timestamps = cooldowns.get(command.name);
+    const cooldownAmount = (command.cooldown || 3) * 1000;
+    if (timestamps.has(message.author.id)) {
+        const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+
+        if (now < expirationTime) {
+            const timeLeft = (expirationTime - now) / 1000;
+            return message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`);
+        }
+    }
+    timestamps.set(message.author.id, now);
+    setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
 
     try {
         command.execute(message, args)
