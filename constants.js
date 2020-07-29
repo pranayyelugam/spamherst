bf4ff214ed2842275dd77e7930df7804a68ba1c5
Pranay Yelugam <shruthiy@SHRUTHIYs-MacBook-Pro.local>
2020-07-29 21:10:14 +0530

    updated client.js

--- constants.js
+++ constants.js
@@ -4,6 +4,77 @@ const ChannelToggleRepository = [
         messageId: "737802281281650709",
         emoji: ":books:",
         channelId: "737760011115298828",
+        channelName: "coursework",
+        roleId: "UMass"
+    },
+    {
+        messageId: "737802281281650709",
+        emoji: ":moneybag:",
+        channelId: "737760133232197793",
+        channelName: "tuition",
+        roleId: "UMass"
+    },
+    {
+        messageId: "737802281281650709",
+        emoji: ":money_with_wings:",
+        channelId: "737760214736175125",
+        channelName: "loan",
+        roleId: "UMass"
+    },
+    {
+        messageId: "737802281281650709",
+        emoji: ":tickets:",
+        channelId: "737760231546945647",
+        channelName: "visa",
+        roleId: "UMass"
+    },
+    {
+        messageId: "737802281281650709",
+        emoji: ":computer:",
+        channelId: "737760277394620426",
+        channelName: "leetcode",
+        roleId: "UMass"
+    },
+    {
+        messageId: "737802281281650709",
+        emoji: ":homes:",
+        channelId: "737760339965509682",
+        channelName: "housing",
+        roleId: "UMass"
+    },
+    {
+        messageId: "737802281281650709",
+        emoji: ":game_die:",
+        channelId: "737760360962195466",
+        channelName: "politics",
+        roleId: "UMass"
+    },
+    {
+        messageId: "737802281281650709",
+        emoji: ":monkey_face:",
+        channelId: "738057006765834361",
+        channelName: "memes",
+        roleId: "UMass"
+    },
+    {
+        messageId: "737802281281650709",
+        emoji: ":regional_indicator_m:",
+        channelId: "737760532328874024",
+        channelName: "math",
+        roleId: "UMass"
+    },
+    {
+        messageId: "737802281281650709",
+        emoji: ":regional_indicator_p:",
+        channelId: "737760564239138990",
+        channelName: "personal-projects",
+        roleId: "UMass"
+    },
+    {
+        messageId: "737802281281650709",
+        emoji: ":man_student_tone1:",
+        channelId: "737760618953703544",
+        channelName: "final-year-peeps",
         roleId: "UMass"
     }
 ]
